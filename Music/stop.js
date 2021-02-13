562a2822a239a9abf35811b6ba0c7a2f8ec9a6ea
Ocean YT <67411162+OceaNKumaR@users.noreply.github.com>
2021-02-13 14:19:17 +0530

    Update stop.js

--- Music/stop.js
+++ Music/stop.js
@@ -11,7 +11,7 @@ const { PREFIX } = require(`../config.json`);
 module.exports = {
   name: "stop",
   description: "Stops the music",
-  aliases: ["leave", "end"],
+  aliases: ["leave", "end", "dc"],
   cooldown: 5,
   edesc: `Type the Command, to stop playing and leave the channel.\nUsage: ${PREFIX}stop`,
 
